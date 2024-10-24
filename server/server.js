d8db613972a4e45a4200bed0dc1e0ee322be4a1d
gursahaj349 <gursahaj349.be22@chitkara.edu.in>
2024-10-24 09:52:11 +0530

    Boilerplate - HBS

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require(".middleware/errorHandler");
+const cors= require("cors");
+
+//env file config
+const dotenv = require("dotenv");
+dotenv.config();      
+
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(core());
+
+
+//ROUTES BELOW
+app.length('/',(req,res)=>{
+    res.send("Working");
+});
+
+
+//APP CONFIG START
+app.listen(port,()=>{
+    console.log(`server running on port https://localhost:${port}`);
+})
